78db9421fd9e6f2a1f714c6fa62eb4ac4d0d969e
Scott Olsen <scg.olsen@gmail.com>
2018-11-06 17:08:12 -0500

    Refactor Lodes and Executors. Add Equality Tests.
    Because of Crystal's current type grammar limitations, specifically the
    handling of generics, I've refactored the design to place fewer
    constraints on consumers. The Executor class no longer requires a
    receiver. However, a Lode does require a comparable executor, binding
    users to the Executor API.

--- spec/motherlode_spec.cr
+++ spec/motherlode_spec.cr
@@ -19,12 +19,27 @@ describe Motherlode do
 
   describe "Lode" do
     lode = Motherlode::Lode.new("lode", Motherlode::NilExecutor.new)
-    
+    lode_two = Motherlode::Lode.new("lode", Motherlode::NilExecutor.new)
+    lode_three = Motherlode::Lode.new("lode", Motherlode::FalseExecutor.new)
+    lode_four = Motherlode::Lode.new("doop", Motherlode::FalseExecutor.new) 
+
     it "Initializes." do
       lode.should be_a(Motherlode::Lode)
     end
-
-
+  
+    it "Should be equal to itself." do
+      t = lode == lode_two 
+      t.should be_true
+    end
+    
+    it "Should not be equal to another lode with a different executor." do
+      t = lode == lode_three
+      t.should be_false
+    end
+    
+    it "Should not be equal to another lode with a different command." do
+      t = lode_four == lode_three
+      t.should be_false
+    end
   end
-
 end
--- src/motherlode.cr
+++ src/motherlode.cr
@@ -1,21 +1,17 @@
-# TODO: Write documentation for `Motherlode`
-module Motherlode
+# TODO: Write documentation for `Motherlode` module Motherlode
+module Motherlode 
   VERSION = "0.1.0"
   extend self
   
-  @@lodes = Set(Lode).new
+  @@lodes = nil
   
   def register(lode : Lode) 
     @@lodes << lode
   end
 
-  def mine(lode : Lode)
-    lode.executor.execute
-  end
-  
   class Lode
     getter command, executor
-    def initialize(@command : String, @executor : Executor(T)) forall T
+    def initialize(@command : String, @executor : Executor)
     end
 
     def ==(other : self)
@@ -23,11 +19,7 @@ module Motherlode
     end
   end
 
-  abstract class Executor(T)
-    getter reciever
-    def initialize(@reciever : T)
-    end
-    
+  abstract class Executor
     def ==(other : self) 
       self.class == other.class
     end
@@ -35,13 +27,15 @@ module Motherlode
     abstract def execute
   end
 
-  class NilExecutor < Executor(Nil)
-    def initialize()
-      super(nil)
-    end
-    
+  class NilExecutor < Executor
     def execute
       return nil
+    end
+  end
+
+  class FalseExecutor < Executor
+    def execute
+      false
     end
   end
 
