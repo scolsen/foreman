957d36d6ab12b43fa7ce63e0dc36718b311c595e
Scott Olsen <scg.olsen@gmail.com>
2018-11-09 15:50:39 -0500

    Use struct for payloads.

--- src/motherlode/payload.cr
+++ src/motherlode/payload.cr
@@ -2,7 +2,7 @@ module Motherlode
   alias Simple = Nil | Bool | String | Char | Symbol | Int32
   alias SimpleClass = Nil.class | Bool.class | String.class | Char.class | Symbol.class | Int32.class
   
-  class Payload
+  struct Payload
     getter chip : Chip.class,
            type : SimpleClass,
            data : Simple
