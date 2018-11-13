47b8f1cf2d74b68bc931225cf9f3784dbeebb820
Scott Olsen <scg.olsen@gmail.com>
2018-11-13 16:41:56 -0500

    Refactor miner. Remove lode, chip, and payload.

--- src/foreman/miner.cr
+++ src/foreman/miner.cr
@@ -1,43 +1,78 @@
 require "option_parser"
 
 module Foreman
-  class Miner
-    getter parser : OptionParser
-    getter command : String
-    getter results : Array(Payload)
-    getter help : String | Nil
+  abstract class Miner
+    getter parser  : OptionParser,
+           command : String,
+           options : Hash(String, String | Bool),
+           free_args : Array(String)
 
-    def initialize(@command : String)
-      @parser = OptionParser.new
-      @results = [] of Payload
+    
+    abstract def execute(values : Array(String))
+
+    private def set_option(opt : String, val : String | Bool)
+      options[opt] = val
     end
-
-    def initialize(@command : String, @help : String)
-      @parser = OptionParser.new
-      @results = [] of Payload
-    end
-
-    def mine(args)
+def mine(args)
       @parser.parse(args)
+      execute @free_args
     end
 
-    def add(short : String, long : String, description : String, lode : Lode)
-      parser.on(short, long, description) do | data |
-        unless data.empty?
-          @results << lode.compute_with(data)
-        else
-          @results << lode.compute
+    def get_option(opt : String)
+      return if @options[opt]?.nil?
+      @options[opt]
+    end
+
+    def make_opt(short : String, long : String, arg : String)
+      sopt = "-"  + short.strip
+      sopt = sopt + arg.upcase
+      lopt = "--" + long.strip
+      lopt = lopt + arg.upcase
+      return {sopt, lopt}
+    end
+    
+    def make_opt(short : String, long : String)
+      sopt = "-"  + short.strip
+      lopt = "--" + long.strip
+      return {sopt, lopt}
+    end
+    
+    def option(short : String, long : String, description : String, arg : String)
+      opts = make_opt(short, long, arg)
+      parser.on(opts[0], opts[1], description) do | data |
+        if data.empty?
+          set_option(long, true)
+        else  
+          set_option(long, data)
         end
       end
     end
 
-    def default(lode : Lode)
-      parser.unknown_args do | data |
-        unless data.empty?
-          @results << lode.compute_with(data)
-        else
-          @results << lode.compute
+    def option(short : String, long : String, description : String)
+      opts = make_opt(short, long)
+      parser.on(opts[0], opts[1], description) do | data |
+        if data.empty?
+          set_option(long, true)
+        else  
+          set_option(long, data) 
         end
+      end
+    end
+
+    def initialize(@command : String)
+      @free_args = [] of String
+      @options = {} of String => String | Bool
+      @parser = OptionParser.new
+      @parser.unknown_args do | data |
+        @free_args = data
+      end
+      @parser.missing_option do | data |
+        puts "Option #{data} is missing an argument." 
+        exit 1
+      end
+      @parser.invalid_option do | data |
+        puts "Invalid option: #{data}"
+        exit 1
       end
     end
   end
--- test.cr
+++ test.cr
@@ -1,11 +1,22 @@
 require "./src/foreman"
 
-lode = Foreman::Lode.new([Foreman::NilChip, Foreman::FalseChip, Foreman::NilChip, Foreman::Util::BooleanOptionChip])
+class MyMiner < Foreman::Miner
+  def execute(data : Array(String))
+    puts "Hello world."
+    puts get_option("nil")
+    puts @options
+    puts Foreman.config
+  end
+end
 
-min = Foreman::Miner.new("test")
-min.add("-n", "--nil", "Returns nil.", lode) 
-min.default(lode)
+min = MyMiner.new("test")
+min.option("n", "nil", "Returns nil.") 
 
+config = [{option: "test", default: "foo"},
+          {option: "other", default: "bar"}]
+
+Foreman.default_config(config)
 Foreman.register(min)
 Foreman.parse
 
+puts Dir.current
